0b280e44daefe40c61b958cbf294b98fa9ce19db
caiiiac <caiiiac@163.com>
2017-09-26 18:38:59 +0800

    rootscene

---
+++ RNMeiTuan/src/RootScene.js
@@ -0,0 +1,7 @@
+/**
+ * Created by caiiiac on 2017/9/26.
+ */
+
+import React, { PureComponent } from 'react'
+import { StatusBar } from 'react-native'
+import { StackNavigator, TabNavigator, TabBarBottom } from 're'

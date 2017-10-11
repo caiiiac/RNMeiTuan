43df39b2475bdded909c570296dffb9ea86b44fc
caiiiac <caiiiac@163.com>
2017-10-11 17:26:00 +0800

    screen

--- RNMeiTuan/src/common/screen.js
+++ RNMeiTuan/src/common/screen.js
@@ -1,3 +1,12 @@
 /**
  * Created by caiiiac on 2017/10/11.
  */
+
+import { Dimensions, Platform, PixelRatio} from 'react-native'
+
+export default {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    onePixe: 1 / PixelRatio.get(),
+    statusBarHeight: (Platform.OS === 'ios' ? 20 : 0)
+}

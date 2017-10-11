20af3e5cac76d5bc48eb002dc972dab18cd366f5
caiiiac <caiiiac@163.com>
2017-10-11 17:27:34 +0800

    import color

--- RNMeiTuan/src/RootScene.js
+++ RNMeiTuan/src/RootScene.js
@@ -4,4 +4,7 @@
 
 import React, { PureComponent } from 'react'
 import { StatusBar } from 'react-native'
-import { StackNavigator, TabNavigator, TabBarBottom } from 're'
+import { StackNavigator, TabNavigator, TabBarBottom } from 'react-navigation'
+
+import color from './widget/color'
+import { screen, system, tool } from './common'

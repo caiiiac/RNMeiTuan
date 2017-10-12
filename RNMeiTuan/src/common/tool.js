edab4c0e05dad1bc00436b07bf71bf6049fc9bf9
caiiiac <caiiiac@163.com>
2017-10-12 15:59:49 +0800

    tool

--- RNMeiTuan/src/common/tool.js
+++ RNMeiTuan/src/common/tool.js
@@ -1,3 +1,18 @@
 /**
  * Created by caiiiac on 2017/10/11.
  */
+
+export function urlByAppendingParams(url: string, params: Object) {
+    let result = url
+    if (result.substr(result.length - 1) != '?') {
+        result = result + '?'
+    }
+
+    for (let key in params) {
+        let value = params[key]
+        result += `${encodeURIComponent(key)}=${encodeURIComponent(value)}&`
+    }
+
+    result = result.substring(0, result.length - 1);
+    return result;
+}

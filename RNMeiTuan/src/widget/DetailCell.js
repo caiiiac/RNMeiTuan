96c76e455eb71f7f77097c56b0afdb1c5b851ea5
caiiiac <caiiiac@163.com>
2017-10-16 15:05:23 +0800

    add DetailCell

---
+++ RNMeiTuan/src/widget/DetailCell.js
@@ -0,0 +1,8 @@
+/**
+ * Created by caiiiac on 2017/10/16.
+ */
+
+import React, { PureComponent } from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { Heading1, Heading2, Paragraph } from './Text'
+import

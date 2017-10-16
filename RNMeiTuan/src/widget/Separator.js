a82cb372b1d5be05c16c56c3031d4a22738e8e76
caiiiac <caiiiac@163.com>
2017-10-16 15:10:39 +0800

    class Separator

--- RNMeiTuan/src/widget/Separator.js
+++ RNMeiTuan/src/widget/Separator.js
@@ -1,3 +1,28 @@
 /**
  * Created by caiiiac on 2017/10/16.
  */
+
+
+import React, { PureComponent } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+import color from './color'
+import { screen, system, tool } from '../common'
+
+class Separator extends PureComponent {
+    render() {
+        return (
+            <View style={[ styles.line, this.props.style ]} />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    line: {
+        width: screen.width,
+        height: screen.onePixe,
+        backgroundColor: color.border,
+    },
+});
+
+export default Separator;

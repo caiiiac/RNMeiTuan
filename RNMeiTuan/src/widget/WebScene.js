4d98d7b8dd6e4060dfb71f0afbcbf184fb289261
caiiiac <caiiiac@163.com>
2017-10-24 17:06:58 +0800

    WebScene

--- RNMeiTuan/src/widget/WebScene.js
+++ RNMeiTuan/src/widget/WebScene.js
@@ -1,3 +1,66 @@
 /**
  * Created by caiiiac on 2017/10/24.
  */
+
+import React, { PureComponent } from 'react'
+import { View, Text, StyleSheet, WebView, InteractionManager } from 'react-native'
+
+class WebScene extends PureComponent {
+
+    static navigationOptions = ({ navigation }) => ({
+        headerStyle: { backgroundColor: 'white' },
+        title: navigation.state.params.title,
+    });
+
+    state: {
+        source: Object
+    }
+
+    constructor(props: Object) {
+        super(props)
+        this.state = {
+            source: {}
+        }
+    }
+
+    componentDidMount() {
+        InteractionManager.runAfterInteractions(() => {
+            this.props.navigationOptions.setParams({ title: '加载中' })
+            this.setState({ source: {uri: this.props.navigation.state.params.url } })
+        })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <WebScene
+                    ref='webView'
+                    automaticallyAdjustContentInsets={false}
+                    style={styles.webView}
+                    source={this.state.source}
+                    onLoadEnd={(e) => this.onLoadEnd(e)}
+                    scalesPageToFit
+                />
+            </View>
+        );
+    }
+
+    onLoadEnd(e: any) {
+        if (e.nativeEvent.title.length > 0) {
+            this.props.navigation.setParams({ title: e.nativeEvent.title })
+        }
+    }
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#2c3e50',
+    },
+    webView: {
+        flex: 1,
+        backgroundColor: 'white',
+    }
+});
+
+export default WebScene;

6b5985e92863fc1b8b32843a128551892d609c1a
caiiiac <caiiiac@163.com>
2017-10-25 15:09:16 +0800

    SpacingView

--- RNMeiTuan/src/widget/SpacingView.js
+++ RNMeiTuan/src/widget/SpacingView.js
@@ -1,3 +1,27 @@
 /**
  * Created by caiiiac on 2017/10/25.
  */
+
+import React, { PureComponent } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+import color from './color'
+
+class SpacingView extends PureComponent {
+    render() {
+        return (
+            <View style={styles.container}>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 14,
+        backgroundColor: color.background,
+    },
+
+});
+
+export default SpacingView;

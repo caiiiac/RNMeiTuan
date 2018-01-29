d18b64c70245b7b18e8ed3a7623442c98b619c43
蔡军强 <caiiiac@163.com>
2018-01-29 16:25:03 +0800

    add Scene

---
+++ RNMeiTuan/src/scene/Mine/MineScene.js
@@ -0,0 +1,13 @@
+/**
+ * Created by caiiiac on 2018/1/29.
+ */
+
+import React, { PureComponent } from 'react'
+
+class MineScene extends PureComponent {
+    render() {
+        return (<view></view>)
+    };
+}
+
+export default MineScene;
---
+++ RNMeiTuan/src/scene/Nearby/NearbyScene.js
@@ -0,0 +1,13 @@
+/**
+ * Created by caiiiac on 2018/1/29.
+ */
+
+import React, { PureComponent } from 'react'
+
+class NearbyScene extends PureComponent {
+    render() {
+        return (<view></view>)
+    };
+}
+
+export default NearbyScene;

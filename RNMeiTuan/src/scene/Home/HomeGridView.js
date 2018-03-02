bb051c975a3196e7b9531c6e3cad2d851a34f47d
蔡军强 <caiiiac@163.com>
2018-03-02 17:36:31 +0800

    c HomeGridView

--- RNMeiTuan/src/scene/Home/HomeGridView.js
+++ RNMeiTuan/src/scene/Home/HomeGridView.js
@@ -1,3 +1,41 @@
 /**
  * Created by caiiiac on 2018/3/2.
  */
+
+import React, { PureComponent } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { color } from '../../widget'
+import { screen, system, tool } from '../../common'
+import HomeGridItem from './HomeGridItem'
+
+class HomeGridView extends PureComponent {
+    static defaultProps = {
+        infos: []
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {this.props.info.map((info, index) => (
+                    <HomeGridItem
+                        info={info}
+                        key={index}
+                        onPress={() => this.props.onGridSelected(index)} />
+                ))}
+            </View>
+        );
+    }
+}
+
+const styles = SytleSheet.create({
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        borderTopWidth: screen.onePixe,
+        borderLeftWidth: screen.onePixe,
+        borderColor: color.border
+    },
+});
+
+export default HomeGridView;
